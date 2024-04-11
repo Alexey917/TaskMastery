a1ff7213f75fbe6c34a389d392aee34c00049d22
Alexey917 <pochtovii.1996@mail.ru>
2024-04-11 12:59:11 +0300

    Added section team work. Fixed output of text information

--- TaskMastery/task_mastery/views.py
+++ TaskMastery/task_mastery/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'task_mastery/base.html')
+    planning = [
+        'систематизируйте свои проекты', 
+        'создавайте четкие планы действий', 
+        'отслеживайте прогресс в реальном времени'
+        ]
+    
+    siteIntended = [
+        'повысить свою продуктивность',
+        'легко планировать и отслеживать свои дела',
+        'а также эффективно координировать работу в команде'
+    ]
+
+    teamWork = [
+        'работайте с коллегами над общими проектами',
+        'делегируйте задачи',
+        'обменивайтесь комментариями и файлами для более эффективного сотрудничества'
+    ]
+
+    return render(request, 'task_mastery/base.html', context={"planning": planning, 
+                                                              "siteIntended": siteIntended,
+                                                              "teamWork": teamWork})
+
