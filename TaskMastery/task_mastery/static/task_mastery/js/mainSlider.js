67cba66296c1b66a4cc1cbb3207868dc9af7401f
Alexey917 <pochtovii.1996@mail.ru>
2024-04-12 21:33:05 +0300

    Фdded a scrolling effect to the slider

--- TaskMastery/task_mastery/static/task_mastery/js/mainSlider.js
+++ TaskMastery/task_mastery/static/task_mastery/js/mainSlider.js
@@ -7,34 +7,37 @@ const sliderBtnPrev = document.querySelector(".slider-btn-prev");
 let counter = 0;
 const slider = [];
 
-sliderBtnPrev.classList.add("section-hiden");
+sliderBtnPrev.classList.add("section-hidden");
 
 function checkSlider() {
   if (counter === section.length - 1) {
-    sliderBtnNext.classList.add("section-hiden");
+    sliderBtnNext.classList.add("section-hidden");
   } else {
-    sliderBtnNext.classList.remove("section-hiden");
+    sliderBtnNext.classList.remove("section-hidden");
   }
 
   if (counter === 0) {
-    sliderBtnPrev.classList.add("section-hiden");
+    sliderBtnPrev.classList.add("section-hidden");
   } else {
-    sliderBtnPrev.classList.remove("section-hiden");
+    sliderBtnPrev.classList.remove("section-hidden");
   }
 }
 
 function displaySection() {
   for (let i = 0; i < section.length; i++) {
-    i !== counter
-      ? section[i].classList.add("section-hiden")
-      : section[i].classList.remove("section-hiden");
+    if (i !== counter) {
+      section[i].classList.add("disapearence");
+      section[i].classList.remove("appearence");
+    } else {
+      section[i].classList.add("appearence");
+      section[i].classList.remove("disapearence");
+    }
   }
 }
 
 displaySection();
 
 sliderBtnNext.addEventListener("click", () => {
-  section[counter].classList.add("disappearence");
   counter++;
   displaySection();
   console.log(counter);
