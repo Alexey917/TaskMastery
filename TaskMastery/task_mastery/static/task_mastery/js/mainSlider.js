5c7d0c99afae13f69e96a1bf8b494e84406fa368
Alexey917 <pochtovii.1996@mail.ru>
2024-04-12 19:15:01 +0300

    Fixed the indentation of the text and elements inside the container

--- TaskMastery/task_mastery/static/task_mastery/js/mainSlider.js
+++ TaskMastery/task_mastery/static/task_mastery/js/mainSlider.js
@@ -3,38 +3,38 @@ const section = document.querySelectorAll(".main-section");
 const sliderBtnNext = document.querySelector(".slider-btn-next");
 const sliderBtnPrev = document.querySelector(".slider-btn-prev");
 
+
 let counter = 0;
 const slider = [];
 
-
-sliderBtnPrev.classList.add("section-hiiden");
+sliderBtnPrev.classList.add("section-hiden");
 
 function checkSlider() {
   if (counter === section.length - 1) {
-    sliderBtnNext.classList.add("section-hiiden");
+    sliderBtnNext.classList.add("section-hiden");
   } else {
-    sliderBtnNext.classList.remove("section-hiiden");
+    sliderBtnNext.classList.remove("section-hiden");
   }
 
   if (counter === 0) {
-    sliderBtnPrev.classList.add("section-hiiden");
+    sliderBtnPrev.classList.add("section-hiden");
   } else {
-    sliderBtnPrev.classList.remove("section-hiiden");
+    sliderBtnPrev.classList.remove("section-hiden");
   }
 }
 
 function displaySection() {
   for (let i = 0; i < section.length; i++) {
     i !== counter
-      ? section[i].classList.add("section-hiiden")
-      : section[i].classList.remove("section-hiiden");
+      ? section[i].classList.add("section-hiden")
+      : section[i].classList.remove("section-hiden");
   }
 }
 
 displaySection();
 
 sliderBtnNext.addEventListener("click", () => {
-  section[counter].classList.add("appearance");
+  section[counter].classList.add("disappearence");
   counter++;
   displaySection();
   console.log(counter);
