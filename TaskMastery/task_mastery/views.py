9ee86282209cafd55f498db7f57a0a1b869050bf
Alexey917 <pochtovii.1996@mail.ru>
2024-04-23 03:38:52 +0300

    Added an addition to the projects database

--- TaskMastery/task_mastery/views.py
+++ TaskMastery/task_mastery/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Projects
 
 
 def register(request):
